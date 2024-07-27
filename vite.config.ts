9af60c943ec8f08a20435a28fbc2beb9622ab400
Charlie-Gauvin <gauvinch9@gmail.com>
2024-07-27 16:48:38 +0200

    feat: :sparkles: Set Dockerfile on this project

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permet les connexions externes
+    port: 5173, // Port par défaut pour Vite
+  },
 });
